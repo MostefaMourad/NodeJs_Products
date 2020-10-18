d70847f8c52d088fcaf3ec6b0b02fa99203c722c
MostefaMourad <hm_mostefa@esi.dz>
2020-10-18 06:38:30 +0100

    CRUD implementation

--- backend/app.js
+++ backend/app.js
@@ -29,10 +29,28 @@ app.post('/api/products', (req, res, next) => {
      .catch( error => res.status(400).json({error}));
 });
 
+app.get('/api/products/:id', (req, res, next) => {
+  Product.findOne({ _id: req.params.id })
+    .then(product => res.status(200).json(product))
+    .catch(error => res.status(404).json({ error }));
+});
+
 app.get('/api/products',(req,res,next) => {
   Product.find()
          .then( products => res.status(200).json({products}))
          .catch( error => res.status(400).json({error})) 
+});
+
+app.put('/api/products/:id', (req, res, next) => {
+  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Modified!' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/products/:id',(req,res,next) => {
+  Product.deleteOne({ _id:req.params.id })
+       .then( () => res.status(200).json({ message: 'Deleted!' }) 
+       .catch( error => res.status(400).json({ error })));
 });
 
 
